951d2b91298dbf8f9295b09189b941255d0a5e21
Mihai CAZACU <cazacugmihai@gmail.com>
2017-10-08 19:28:45 +0300

    removed hardcoded HADOOP_HOME from bootstrap.sh; reformat code

--- hadoop-master/bootstrap.sh
+++ hadoop-master/bootstrap.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-: ${HADOOP_PREFIX:=/usr/local/hadoop}
-
-$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
+$HADOOP_HOME/etc/hadoop/hadoop-env.sh
 
 rm /tmp/*.pid
 
 # installing libraries if any - (resource urls added comma separated to the ACP system variable)
-cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
+cd $HADOOP_HOME/share/hadoop/common ; for cp in ${ACP//,/ }; do echo == $cp; curl -LO $cp ; done; cd -
 
 # altering the core-site configuration
 #sed s/NAMENODE/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
@@ -15,10 +13,11 @@ cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; 
 #sed s/RESOURCEMANAGER/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/yarn-site.xml.template.template > /usr/local/hadoop/etc/hadoop/yarn-site.xml.template
 
 service ssh start
-nohup $HADOOP_PREFIX/bin/hdfs namenode &
-nohup $HADOOP_PREFIX/bin/yarn resourcemanager &
-nohup $HADOOP_PREFIX/bin/yarn timelineserver &
-nohup $HADOOP_PREFIX/bin/mapred historyserver &
+
+nohup $HADOOP_HOME/bin/hdfs   namenode        2>> $HADOOP_LOG_DIR/namenode.err        >> $HADOOP_LOG_DIR/namenode.out &
+nohup $HADOOP_HOME/bin/yarn   resourcemanager 2>> $HADOOP_LOG_DIR/resourcemanager.err >> $HADOOP_LOG_DIR/resourcemanager.out &
+nohup $HADOOP_HOME/bin/yarn   timelineserver  2>> $HADOOP_LOG_DIR/timelineserver.err  >> $HADOOP_LOG_DIR/timelineserver.out &
+nohup $HADOOP_HOME/bin/mapred historyserver   2>> $HADOOP_LOG_DIR/historyserver.err   >> $HADOOP_LOG_DIR/historyserver.out &
 
 if [[ $1 == "-d" ]]; then
     while true; do sleep 1000; done
--- hadoop-slave/bootstrap.sh
+++ hadoop-slave/bootstrap.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-: ${HADOOP_PREFIX:=/usr/local/hadoop}
-
-$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
+$HADOOP_HOME/etc/hadoop/hadoop-env.sh
 
 rm /tmp/*.pid
 
 # installing libraries if any - (resource urls added comma separated to the ACP system variable)
-cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
+cd $HADOOP_HOME/share/hadoop/common ; for cp in ${ACP//,/ }; do echo == $cp; curl -LO $cp ; done; cd -
 
 # altering the core-site configuration
 #sed s/NAMENODE/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
@@ -15,13 +13,17 @@ cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; 
 #sed s/RESOURCEMANAGER/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/yarn-site.xml.template.template > /usr/local/hadoop/etc/hadoop/yarn-site.xml.template
 
 service ssh start
-nohup $HADOOP_PREFIX/bin/hdfs datanode 2>> /var/log/hadoop/datanode.err >> /var/log/hadoop/datanode.out &
-nohup $HADOOP_PREFIX/bin/yarn nodemanager 2>> /var/log/hadoop/nodemanager.err >> /var/log/hadoop/nodemanager.out &
+
+nohup $HADOOP_HOME/bin/hdfs datanode    2>> $HADOOP_LOG_DIR/datanode.err    >> $HADOOP_LOG_DIR/datanode.out &
+nohup $HADOOP_HOME/bin/yarn nodemanager 2>> $HADOOP_LOG_DIR/nodemanager.err >> $HADOOP_LOG_DIR/nodemanager.out &
 
 if [[ $1 == "-d" ]]; then
     while true; do sleep 1000; done
 fi
 
 if [[ $1 == "-bash" ]]; then
+    /bin/bash
+fi
+ $1 == "-bash" ]]; then
     /bin/bash
 fi
